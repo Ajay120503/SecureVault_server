249656be586e6419196b244b6a60c2353f423134
Ajay120503 <ajaykandhare12@gmail.com>
2026-02-20 15:12:44 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -7,24 +7,7 @@ const rateLimit = require("./middleware/rateLimit.middleware");
 
 const app = express();
 
-const allowedOrigins = process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL.split(",")
-  : ["http://localhost:5173"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(morgan("dev"));
