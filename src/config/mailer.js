cff51c11dd5a7048cc9456bf779512b0e51992d4
Ajay120503 <ajaykandhare12@gmail.com>
2026-02-21 15:28:26 +0530

    first commit

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -14,7 +14,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -22,6 +22,7 @@ const transporter = nodemailer.createTransport({
   connectionTimeout: 15000,
   greetingTimeout: 15000,
   socketTimeout: 20000,
+  family: 4,
   tls: {
     rejectUnauthorized: false,
   },
